08ff93f8147a11587bf3fcd23e83dacc57153edb
Udo Bretz <udo.bretz@otrs.com>
2010-08-09 07:02:59 +0000

    Renamed Kernel::System::Config into Kernel::System::SysConfig in OTRS 3.0.x.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -2,7 +2,7 @@
 # ITSM.pm - code to excecute during package installation
 # Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: ITSM.pm,v 1.2 2010-07-26 23:41:19 ub Exp $
+# $Id: ITSM.pm,v 1.3 2010-08-09 07:02:59 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,9 +15,10 @@ use strict;
 use warnings;
 
 use Kernel::System::Package;
+use Kernel::System::SysConfig;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 =head1 NAME
 
@@ -92,7 +93,7 @@ sub new {
     }
 
     # create needed sysconfig object
-    $Self->{SysConfigObject} = Kernel::System::Config->new( %{$Self} );
+    $Self->{SysConfigObject} = Kernel::System::SysConfig->new( %{$Self} );
 
     # rebuild ZZZ* files
     $Self->{SysConfigObject}->WriteDefault();
@@ -133,7 +134,7 @@ sub new {
     ];
 
     # define the version of the included packages
-    $Self->{PackageVersion} = '2.0.91';
+    $Self->{PackageVersion} = '2.1.91';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
@@ -504,6 +505,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/gpl-2.0.txt>.
 
 =head1 VERSION
 
-$Revision: 1.2 $ $Date: 2010-07-26 23:41:19 $
+$Revision: 1.3 $ $Date: 2010-08-09 07:02:59 $
 
 =cut
