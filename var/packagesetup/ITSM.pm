37e19c08b1110edcb5d25b85ed3bd4736c46e2ad
Udo Bretz <udo.bretz@otrs.com>
2013-07-03 12:56:52 +0000

    Updated version numbers to 3.1.10.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -2,7 +2,7 @@
 # ITSM.pm - code to excecute during package installation
 # Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: ITSM.pm,v 1.26.2.2 2013-05-22 07:38:40 mh Exp $
+# $Id: ITSM.pm,v 1.26.2.3 2013-07-03 12:56:52 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -18,7 +18,7 @@ use Kernel::System::Package;
 use Kernel::System::SysConfig;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.26.2.2 $) [1];
+$VERSION = qw($Revision: 1.26.2.3 $) [1];
 
 =head1 NAME
 
@@ -134,7 +134,7 @@ sub new {
     ];
 
     # define the version of the included packages
-    $Self->{PackageVersion} = '3.1.9';
+    $Self->{PackageVersion} = '3.1.10';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
@@ -505,6 +505,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/gpl-2.0.txt>.
 
 =head1 VERSION
 
-$Revision: 1.26.2.2 $ $Date: 2013-05-22 07:38:40 $
+$Revision: 1.26.2.3 $ $Date: 2013-07-03 12:56:52 $
 
 =cut
