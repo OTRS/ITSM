59943f286e9c64976f982c817a3291bcf48c9c53
Udo Bretz <udo.bretz@otrs.com>
2013-08-23 15:56:59 +0200

    Tidied code.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -1,13 +1,13 @@
 # --
 # ITSM.pm - code to excecute during package installation
-# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package var::packagesetup::ITSM;
+package var::packagesetup::ITSM;    ## no critic
 
 use strict;
 use warnings;
@@ -492,7 +492,7 @@ sub _CheckVersion {
 
 =head1 TERMS AND CONDITIONS
 
-This software is part of the OTRS project (http://otrs.org/).
+This software is part of the OTRS project (L<http://otrs.org/>).
 
 This software comes with ABSOLUTELY NO WARRANTY. For details, see
 the enclosed file COPYING for license information (AGPL). If you
