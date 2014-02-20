959dab94ddd9fdd46c7a1ca753830fb2f64803c1
Udo Bretz <udo.bretz@otrs.com>
2014-02-20 12:35:21 +0100

    Updated version number to 3.3.5.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -129,7 +129,7 @@ sub new {
     ];
 
     # define the version of the included packages
-    $Self->{PackageVersion} = '3.3.4';
+    $Self->{PackageVersion} = '3.3.5';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
