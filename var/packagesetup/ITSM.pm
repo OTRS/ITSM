61e4388f621bc9a32a567d93c1fff0e606b4e63e
Udo Bretz <udo.bretz@otrs.com>
2015-05-06 16:01:25 +0200

    Updated version numbers to 4.0.8.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -94,7 +94,7 @@ sub new {
     ];
 
     # define the version of the included packages
-    $Self->{PackageVersion} = '4.0.7';
+    $Self->{PackageVersion} = '4.0.8';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
