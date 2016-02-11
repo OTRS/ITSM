c2809f308436a50320d3cfc3c9713bb558d456fc
Udo Bretz <udo.bretz@otrs.com>
2016-02-11 10:35:27 +0100

    Updated version numbers to 5.0.7.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -93,7 +93,7 @@ sub new {
     ];
 
     # define the version of the included packages
-    $Self->{PackageVersion} = '5.0.6';
+    $Self->{PackageVersion} = '5.0.7';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
