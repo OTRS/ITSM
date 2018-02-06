f69ee28ce3f99060c622e2b707439d6ab3648b2c
Udo Bretz <udo.bretz@otrs.com>
2018-02-06 11:19:29 +0100

    Updated version numbers to 6.0.5.

--- var/packagesetup/ITSM.pm
+++ var/packagesetup/ITSM.pm
@@ -66,7 +66,7 @@ sub new {
     ];
 
     # define the version of the included packages
-    $Self->{PackageVersion} = '6.0.4';
+    $Self->{PackageVersion} = '6.0.5';
 
     # define miminum required itsm version (if installed already)
     $Self->{MinimumITSMVersion} = '1.3.1';
